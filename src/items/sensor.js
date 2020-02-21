48da8af8a4f756e35b664591cc8d592a1e5675cc
Niklas Sandström <mr.n.sandstrom@gmail.com>
2020-02-22 00:25:24 +0100

    use the item base component for sensor

--- src/items/sensor.js
+++ src/items/sensor.js
@@ -1,48 +1,7 @@
 import React, { useState } from 'react'
+import { Item } from './item'
 import './items.scss'
 
 export const Sensor = ({ sensor }) => {
-  const state = sensor.state
-
-  const [expanded, setExpanded] = useState(false)
-
-  const toggleExpanded = () => {
-    setExpanded(!expanded)
-  }
-
-  return (
-    <div className="item" onClick={toggleExpanded}>
-      {sensor.id} <span className="title">{sensor.name}</span> {sensor.type}{' '}
-      <ExpandButton expanded={expanded} />
-      {expanded && <ExtraInfo sensor={sensor} />}
-    </div>
-  )
-}
-
-const ExpandButton = ({ expanded }) => {
-  return <span className="arrow"> {expanded ? '/\\' : '\\/'}</span>
-}
-
-const ExtraInfo = ({ sensor }) => {
-  const { config, state, ...rest } = sensor
-
-  return (
-    <div className="details">
-      {Object.entries(rest).map(([key, value]) => {
-        return (
-          <div key={key}>
-            {key} : {printValue(value)}
-          </div>
-        )
-      })}
-    </div>
-  )
-}
-
-const printValue = value => {
-  if (typeof value == 'boolean') {
-    return value ? 'true' : 'false'
-  }
-
-  return value
+  return <Item item={sensor}></Item>
 }
